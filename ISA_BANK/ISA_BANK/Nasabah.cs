1569c4215e5fdc49abed207be0a151a10f569c11
mathewrs04 <164150651+mathewrs04@users.noreply.github.com>
2024-04-15 14:10:03 +0700

    tes 1

---
+++ ISA_BANK/ISA_BANK/Nasabah.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BANK.Class___DB
+{
+    internal class Nasabah
+    {
+        #region datamember
+        private string id;
+        private string nama;
+        private DateTime ttl;
+        private string nik;
+        private string no_telepon;
+        #endregion
+
+        #region constructor
+        public Nasabah(string id, string nama, DateTime ttl, string nik, string no_telepon)
+        {
+            this.Id = id;
+            this.Nama = nama;
+            this.Ttl = ttl;
+            this.Nik = nik;
+            this.No_telepon = no_telepon;
+        }
+        #endregion
+
+        //test tast test
+        #region properties
+        public string Id { get => id; set => id = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public DateTime Ttl { get => ttl; set => ttl = value; }
+        public string Nik { get => nik; set => nik = value; }
+        public string No_telepon { get => no_telepon; set => no_telepon = value; }
+        #endregion
+
+        #region method
+
+
+
+        #endregion
+    }
+}
